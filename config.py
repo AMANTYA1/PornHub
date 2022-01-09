7ed9d67c54e13b05ed2a954b42581ebdf1330abc
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-09 18:56:19 +0530

    Umm

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", "")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "")
+OWNER = os.environ.get("OWNER", "")
+BOT_NAME = os.environ.get("BOT_NAME", "")
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", " ")
